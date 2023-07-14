63e40ba24e8156abfc6d3110ebc2b1a0a748fe79
acri <eea8024@gmail.com>
2023-07-14 11:06:22 -0300

    Major changes to excerpt implementation. Added checks for design matrix factors to ensure consistency with count matrices and fixes to shell commands in excerpt.sh

--- excerpt.sh
+++ excerpt.sh
@@ -1,7 +1,7 @@
 #docker is runing only with sudo
-#using hg38 genome in the 10.253.0.234 server
-#If using off-site, the path for the exceRptInput may need to be changed (/mnt/acri7/excerpt_in)
+#using hg38 genome in the 10.111.243.103 server
 inFile=$1
-outDir=$2
-sudo docker run -v /mnt/acri7/excerpt_in:/exceRptInput -v ${outDir}:/exceRptOutput -v /references/ExceRptDatabase/hg38:/exceRpt_DB/hg38 -t rkitchen/excerpt INPUT_FILE_PATH=/exceRptInput/${inFile} MAIN_ORGANISM_GENOME_ID=hg38 ADAPTER_SEQ=AAAAAAAA MIN_READ_LENGTH=15 STAR_outFilterMatchNmin=15 RANDOM_BARCODE_LENGTH=4 RANDOM_BARCODE_LOCATION='-5p'
+inDir=$2
+outDir=$3
+sudo docker run -v ${inDir}:/exceRptInput -v ${outDir}:/exceRptOutput -v /references/ExceRptDatabase/hg38:/exceRpt_DB/hg38 -t rkitchen/excerpt INPUT_FILE_PATH=/exceRptInput/${inFile} MAIN_ORGANISM_GENOME_ID=hg38 ADAPTER_SEQ=AAAAAAAA MIN_READ_LENGTH=15 STAR_outFilterMatchNmin=15 RANDOM_BARCODE_LENGTH=4 RANDOM_BARCODE_LOCATION='-5p'
 
